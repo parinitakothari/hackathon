2ddb852c8c307bf329cbfe73c2af542ce853ef9e
gnitz <gnit1989@gmail.com>
2021-11-30 19:42:50 +0000

    Added new function and test case to get Top 3 Posts in order of funding

--- post/src/post/post.test.js
+++ post/src/post/post.test.js
@@ -125,6 +125,117 @@ test('Should return a specific post', async () => {
 
 });
 
+
+test('Should return TOP 3 posts by funding', async () => {
+
+    let postController = PostController();
+
+    const posts = [
+        {
+            id: 1,
+            charityId: 1,
+            charityName: 'One charity',
+            mediaId: 1,
+            userId: 1,
+            userName: 'Marko Maric',
+            description: 'blablabla',
+            funds: 5764.2
+        },
+        {
+            id: 2,
+            charityId: 2,
+            charityName: 'Some Charity',
+            mediaId: 2,
+            userId: 2,
+            userName: 'Pero Peric',
+            description: 'Some description',
+            funds: 34.2
+        },
+        {
+            id: 3,
+            charityId: 3,
+            charityName: 'Benevolent Charity',
+            mediaId: 3,
+            userId: 3,
+            userName: 'Pikalo Paputo',
+            description: 'Best Charity',
+            funds: 63.5
+        },
+        {
+            id: 4,
+            charityId: 4,
+            charityName: 'Religious Charity',
+            mediaId: 4,
+            userId: 4,
+            userName: 'Some Person',
+            description: 'Very religious charity',
+            funds: 9999
+        },
+        {
+            id: 5,
+            charityId: 5,
+            charityName: 'Environmental Charity',
+            mediaId: 5,
+            userId: 5,
+            userName: 'MR Greenpeace',
+            description: 'Green is good',
+            funds: 195
+        }
+    ];
+
+    // prepare the reality in the database
+    const conn = typeorm.getConnection();
+    postRepo = await conn.getRepository("Post")
+    result = await postRepo.create(posts);
+    await postRepo.save(result);
+
+    
+    const postsToReturn = [
+        {
+            id: 4,
+            charityId: 4,
+            charityName: 'Religious Charity',
+            mediaId: 4,
+            userId: 4,
+            userName: 'Some Person',
+            description: 'Very religious charity',
+            funds: 9999
+        },
+        {
+            id: 1,
+            charityId: 1,
+            charityName: 'One charity',
+            mediaId: 1,
+            userId: 1,
+            userName: 'Marko Maric',
+            description: 'blablabla',
+            funds: 5764.2
+        },
+        {
+            id: 5,
+            charityId: 5,
+            charityName: 'Environmental Charity',
+            mediaId: 5,
+            userId: 5,
+            userName: 'MR Greenpeace',
+            description: 'Green is good',
+            funds: 195
+        }
+    ];
+
+    const req = expressMock.getMockReq({});
+    const { res, next, mockClear } = expressMock.getMockRes();
+
+    await postController.getTopPostsByFunding(req, res);
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith(postsToReturn);
+
+   /*  outPosts = await conn.getRepository("Post").find({ id: 2 });
+    expect(outPosts.length).toBe(1);
+    expect(outPosts[0]).toStrictEqual(posts[0]);  */
+
+});
+
 test('Should update a specific post', async () => {
     let postController = PostController();
 
@@ -158,7 +269,6 @@ test('Should update a specific post', async () => {
     await postRepo.save(result);
 
     outPosts = await conn.getRepository("Post").find();
-    console.log(outPosts);
 
     const postToUpdate = {
         charityName: 'S C',
@@ -225,7 +335,6 @@ test('Should delete a specific post', async () => {
     await postRepo.save(result);
 
     outPosts = await conn.getRepository("Post");
-    console.log(outPosts);
 
     const postToDelete = {
         id: 2
