f1c4c15641687865ccf203c861948a50ad59bbab
Terry Marsh <terrymarsh2008@hotmail.co.uk>
2021-12-03 12:14:13 +0000

    small change to Dockerfile

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -82,10 +82,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-	analyze: true,
-    	// or
-    	analyze: {
-      		analyzerMode: 'static'
-    	}
   }
 }
